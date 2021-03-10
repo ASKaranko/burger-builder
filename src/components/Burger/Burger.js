3db1ca6da2757c39535da283b8fbe73ec4906df4
Karanko Andrei <andr.karanko@gmail.com>
2021-03-10 02:10:35 +0200

    Burger-builder. Start. Dynamically adding ingredients to a burger

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,24 @@
+import React from "react";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+import classes from './Burger.module.css';
+
+const burger = (props) => {
+	const transformedIngredients = Object.keys(props.ingredients)
+			.map(igKey => {
+				return [...Array(props.ingredients[igKey])].map((_, i) => {
+					return <BurgerIngredient
+							type={igKey}
+							key={igKey + i}
+					/>
+				})
+			});
+	return (
+			<div className={classes.Burger}>
+				<BurgerIngredient type="bread-top" />
+				{transformedIngredients}
+				<BurgerIngredient type="bread-bottom" />
+			</div>
+	);
+};
+
+export default burger;
